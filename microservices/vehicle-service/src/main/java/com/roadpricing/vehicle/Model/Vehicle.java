e8caa5a9428ac8866bdc43bb6b8f98b88f29b3c6
Teun Buis <72912875+347956@users.noreply.github.com>
2023-03-29 11:23:17 +0200

    Update Vehicle.java

--- microservices/vehicle-service/src/main/java/com/roadpricing/vehicle/Model/Vehicle.java
+++ microservices/vehicle-service/src/main/java/com/roadpricing/vehicle/Model/Vehicle.java
@@ -3,10 +3,7 @@ package com.roadpricing.vehicle.Model;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="vehicles")
@@ -14,6 +11,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 public class Vehicle {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name="vehicleModel")
