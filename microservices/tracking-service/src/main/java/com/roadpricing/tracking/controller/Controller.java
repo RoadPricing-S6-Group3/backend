c99679e01abf8b2586eba7fb6f45b4368dc29aec
Teun Buis <72912875+347956@users.noreply.github.com>
2023-05-24 13:48:39 +0200

    Fixed controller EndPoint and removed unused imports

--- microservices/tracking-service/src/main/java/com/roadpricing/tracking/controller/Controller.java
+++ microservices/tracking-service/src/main/java/com/roadpricing/tracking/controller/Controller.java
@@ -1,29 +1,22 @@
 package com.roadpricing.tracking.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.roadpricing.tracking.model.RouteModel;
 import com.roadpricing.tracking.service.RouteService;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.websocket.server.PathParam;
-
 @RestController
 @RequestMapping("/api/tracking")
 public class Controller {
     @Autowired
     RouteService routeService;
-    @Autowired
-    ObjectMapper obejectmapper;
-    @GetMapping("/coord/{coordinates}")
-    public ResponseEntity<RouteModel> createRoute(@PathVariable(value= "coordinates")String coordinates){
-        String cords = "5.480312336981955,51.44370749735556;5.453453331511914,51.45154959663053";
+    @GetMapping("/coord/{coordinatesbegin}/{coordinatesend}")
+    public ResponseEntity<RouteModel> createRoute(@PathVariable("coordinatesbegin")String coordinatesbegin,
+                                                  @PathVariable (value = "coordinatesend")String coordinatesend){
+        String cords = coordinatesbegin + ";" + coordinatesend;
         RouteModel routeModel = new RouteModel();
         try {
             routeModel = routeService.createRoute(cords);
