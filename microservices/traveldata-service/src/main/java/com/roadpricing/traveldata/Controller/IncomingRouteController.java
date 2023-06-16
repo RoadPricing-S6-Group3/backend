f3a46fc49886bdc9bfa3c46579e479029cb042f2
Teun <teunbuis17@gmail.com>
2023-06-16 14:18:01 +0200

    Update IncomingRouteController.java

--- microservices/traveldata-service/src/main/java/com/roadpricing/traveldata/Controller/IncomingRouteController.java
+++ microservices/traveldata-service/src/main/java/com/roadpricing/traveldata/Controller/IncomingRouteController.java
@@ -3,6 +3,8 @@ package com.roadpricing.traveldata.Controller;
 import com.roadpricing.traveldata.Service.RouteInfoService;
 import com.roadpricing.traveldata.dto.IncomingRouteDTO;
 import com.roadpricing.traveldata.dto.OutGoingRouteDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +17,11 @@ public class IncomingRouteController {
 
     @Autowired
     RouteInfoService routeInfoService;
+
+    private static final Logger logger = LoggerFactory.getLogger(IncomingRouteController.class);
     @PostMapping("/submit-raw/{cc}")
     public ResponseEntity<List<OutGoingRouteDTO>> retrieveRoute(@RequestBody IncomingRouteDTO incoming, @PathVariable(value = "cc")String cc){
-
+        logger.info("Received incoming raw-route from country: [ " + cc + " ] [ 🗺️ ]" );
         IncomingRouteDTO recieved = incoming;
         List<OutGoingRouteDTO> outs = routeInfoService.sendProcessRoute(recieved, cc);
         return ResponseEntity.ok().body(outs);
